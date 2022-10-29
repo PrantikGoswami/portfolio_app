a6d6e0e9d8b56fdea6ef6965eba71d2e2bc09b7d
Prantik Goswami <p.goswami@ptc-telematik.de>
2022-10-30 01:59:14 +0200

    Layout creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NgZorroAntdModule } from './ng-zorro-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
