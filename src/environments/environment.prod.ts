612478d30b2bd523a5a5b8cb9c5b5756941f2d58
Prantik Goswami <p.goswami@ptc-telematik.de>
2022-11-22 17:36:36 +0100

    dynamic content from env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,96 @@
 export const environment = {
-  production: true
+  production: true,
+  totalData: [
+    {
+      title : "Java",
+      innerData: [
+        {
+          avatarLink: "https://avatars0.githubusercontent.com/u/22736418?s=88&v=4",
+          contentImageLink: "https://gw.alipayobjects.com/zos/antfincdn/K%24NnlsB%26hz/pageHeader.svg",
+          innerTitle: "Test Title",
+          subTitle: "This is a subtitle",
+          tagText: "Running",
+          tagColor: "blue",
+          contentPara1: "Ant Design interprets the color system into two levels: a system-level color system and a product-level color system.",
+          contentPara2: "Ant Design's design team preferred to design with the HSB color model, which makes it easier for designers to have a clear psychological expectation of color when adjusting colors, as well as facilitate communication in teams.",
+          contentLinkData: [
+            {
+              title : "Quick Start",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg",
+              alt: "start"
+            },
+            {
+              title : "Product Info",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg",
+              alt: "info"
+            },
+            {
+              title : "Product Doc",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg",
+              alt: "doc"
+            }
+          ]
+        },
+        {
+          avatarLink: "https://avatars0.githubusercontent.com/u/22736418?s=88&v=4",
+          contentImageLink: "https://gw.alipayobjects.com/zos/antfincdn/K%24NnlsB%26hz/pageHeader.svg",
+          innerTitle: "Test Title",
+          subTitle: "This is a subtitle",
+          tagText: "Running",
+          tagColor: "blue",
+          contentPara1: "Ant Design interprets the color system into two levels: a system-level color system and a product-level color system.",
+          contentPara2: "Ant Design's design team preferred to design with the HSB color model, which makes it easier for designers to have a clear psychological expectation of color when adjusting colors, as well as facilitate communication in teams.",
+          contentLinkData: [
+            {
+              title : "Quick Start",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg",
+              alt: "start"
+            },
+            {
+              title : "Product Info",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg",
+              alt: "info"
+            },
+            {
+              title : "Product Doc",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg",
+              alt: "doc"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title : "Angular",
+      innerData: [
+        {
+          avatarLink: "https://avatars0.githubusercontent.com/u/22736418?s=88&v=4",
+          contentImageLink: "https://gw.alipayobjects.com/zos/antfincdn/K%24NnlsB%26hz/pageHeader.svg",
+          innerTitle: "Test Title",
+          subTitle: "This is a subtitle",
+          tagText: "Running",
+          tagColor: "blue",
+          contentPara1: "Ant Design interprets the color system into two levels: a system-level color system and a product-level color system.",
+          contentPara2: "Ant Design's design team preferred to design with the HSB color model, which makes it easier for designers to have a clear psychological expectation of color when adjusting colors, as well as facilitate communication in teams.",
+          contentLinkData: [
+            {
+              title : "Quick Start",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg",
+              alt: "start"
+            },
+            {
+              title : "Product Info",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg",
+              alt: "info"
+            },
+            {
+              title : "Product Doc",
+              link: "https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg",
+              alt: "doc"
+            }
+          ]
+        }
+      ]
+    }
+  ]
 };
